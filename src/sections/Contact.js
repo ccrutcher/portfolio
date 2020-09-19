ce6536748d14f2879cef0b88f3a511be378fcafc
Cole Crutcher <colejcrutcher@gmail.com>
2020-09-18 20:12:41 -0500

    renamed class

--- src/sections/Contact.js
+++ src/sections/Contact.js
@@ -4,7 +4,32 @@ import './Contact.css';
 export class Contact extends Component {
   state = {};
   render() {
-    return <div className='contact-section' id='contact'></div>;
+    return (
+      <div className='contact-section' id='contact'>
+        <div className='contact-container'>
+          <div className='contact-outside-links'>
+            <a
+              className='contact-outside-link'
+              href='http://github.com/ccrutcher'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <i className='fab fa-github-square fa-5x contact-github'></i>
+            </a>
+            <a
+              className='contact-outside-link'
+              href='http://linkedin.com'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <i className='fab fa-linkedin fa-5x contact-linkedin'></i>
+            </a>
+          </div>
+          <div className='resume'></div>
+          <div className='email'></div>
+        </div>
+      </div>
+    );
   }
 }
 
